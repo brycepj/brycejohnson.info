d519c1108abb2b1d348c90de75f6ae2f5b3b06b8
Bryce Johnson <bjohnson@ip-192-168-1-4.us-west-2.compute.internal>
2018-02-04 14:45:31 -0500

    Add logs to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+echo "Stopping pm2..."
 pm2 stop 0
+echo "Pulling latest from master..."
 git pull origin master
+echo "Installing frontend dependencies and building for production"
 cd app && yarn && yarn build && cd -
-cd server && yarn 
+echo "Updating server-side dependencies..."
+cd server && yarn
+echo "Restarting pm2"
 pm2 start 0
